3e5e363a966ea2715401d25795b9d906458a5f0a
iaraechenique <iaraechenique0816@gmail.com>
2024-08-28 10:57:56 -0300

    Para solucionar el problema del clic en el div

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById('mi-div').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+document.getElementById('mi-boton').addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation(); // Detiene la propagación del evento al div
+});
